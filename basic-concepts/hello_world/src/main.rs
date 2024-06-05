3656c3c04215a51458fc20e0d0e7015e8ce6d8c1
Hrigved <iamhrigved@gmail.com>
2024-06-05 21:46:17 +0530

    Update basic-concepts section

---
+++ basic-concepts/hello_world/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    println!("Hello, world!"); // `println` that is, print-line can be used to display a hello
+                               // world message.
+                               // notice that there is an exclaimation mark after `println`.
+                               // this is because, this is a `macro` if we remove the exclaimation
+                               // mark, this becomes a function call
+}
